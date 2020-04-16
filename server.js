75f86e10e3baef4013990ca4c169f3db778f60ed
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-04-15 20:58:31 -0700

    Basic inquirer setup and ability to view all tables

--- server.js
+++ server.js
@@ -24,34 +24,95 @@ connection.connect((err) => {
     console.table("connected as id " + connection.threadId);
 });
 
+init();
 
-inquirer
-    .prompt(
+function init() {
+    inquirer
+        .prompt([{
+            type: "list",
 
-    )
-    .then()
+            message: "What would you like to do?",
+
+            choices: ["View All Employees",
+                // "View All Employees By Department",
+                // "View All Employees By Manager",
+                "Add Employee",
+                // "Remove Employee",
+                "Update Employee Role",
+                // "Update Employee Manager",
+                "View All Roles",
+                "Add Role",
+                // "Remove Role",
+                "View All Departments",
+                "Add Department",
+                // "Remove Department"
+            ],
+
+            name: "prompt"
+        }])
+        .then(answers => {
+            console.log(answers)
+            switch (answers.prompt) {
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+                case "Add Role":
+                    addRole();
+                    break;
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                default:
+                    console.log("no worky");
+            };
+
+        });
+}
 
 
+function viewAllEmployees() {
+    connection.query("SELECT * FROM employee;", (err, data) => {
+        if (err) {
+            return res.status(500).end();
+        }
+
+        console.table(data);
+        init();
+    });
+};
 
 
-connection.query("SELECT * FROM department;", (err, data) => {
-    if (err) {
-        return res.status(500).end();
-    }
+function viewAllDepartments() {
+    connection.query("SELECT * FROM department;", (err, data) => {
+        if (err) {
+            return res.status(500).end();
+        }
 
-    console.table(data)
-});
-connection.query("SELECT * FROM role;", (err, data) => {
-    if (err) {
-        return res.status(500).end();
-    }
+        console.table(data);
+        init();
+    });
+}
 
-    console.table(data)
-});
-connection.query("SELECT * FROM employee;", (err, data) => {
-    if (err) {
-        return res.status(500).end();
-    }
 
-    console.table(data)
-});
+function viewAllRoles() {
+    connection.query("SELECT * FROM role;", (err, data) => {
+        if (err) {
+            return res.status(500).end();
+        }
+
+        console.table(data);
+        init();
+    });
+}
